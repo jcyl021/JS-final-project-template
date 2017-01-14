aeeb896b1908e610fc37d6e22c731e99a19ee41a
jcyl021 <liu_jasper@icloud.com>
2017-01-14 15:28:50 +0800

    fuuc

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var bgImg= document.createElement("img")
-bgImg.src= "images/rukia.png"
-var canvas= document.gtElementByld("game-canvas")
-var ctx= canvas.getContext("2d")
+var enemyImg= document.createElement("img");
+bgImg.src= "images/rukia.png";
+var canvas= document.gtElementByld("game-canvas");
+var ctx= canvas.getContext("2d");
 
 function draw (){
-  ctx.drawImage(bgImg,0,0)
+  ctx.drawImage(enemyImg,0,0)
 }
 
-setTimeout (draw, 100)
+setTimeout (draw, 1000)
