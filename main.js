193fea7437c0f5e93502a19cd908c30e965bd67f
jcyl021 <liu_jasper@icloud.com>
2017-01-21 14:48:55 +0800

    yay

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var bgImg= document.createElement("img");
 var enemyImg= document.createElement("img");
 var buttonImg= document.createElement("img");
+var cursorImg= document.createElement("img");
 
 bgImg.src= "images/map.png";
 enemyImg.src= "images/slime.gif";
 buttonImg.src= "images/tower-btn.png";
+cursorImg.src= "images/tower.png";
 
 var canvas= document.getElementById("game-canvas");
 var ctx= canvas.getContext("2d");
@@ -12,7 +14,8 @@ var ctx= canvas.getContext("2d");
 function draw (){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
-    ctx.drawImage(buttonImg,button.x,button.y,100,100)
+    ctx.drawImage(buttonImg,button.x,button.y,100,100);
+    ctx.drawImage(cursorImg,cursor.x,cursor.y)
 }
 
 setInterval (draw, 16)
@@ -25,7 +28,15 @@ var enemy={
    x:540,
    y:380
  }
+ var cursor={
+   x:200,
+   y:25
+ }
 
- $("#target").mousemove(function (event) {
-   console.log("x:" + event.offsetX + ",y:" + event.offsetY);
- });
+ $("#game-canvas").on("mousemove", cursorMove)
+
+function cursorMove(event){
+ cursor.x= event.offsetX;
+ cursor.y= event.offsetY
+ }
+ setInterval (cursorMove, 16)
