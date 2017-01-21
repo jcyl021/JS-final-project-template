b0b2934a7ccfc4d5a3dc24749203efa34b3558a3
jcyl021 <liu_jasper@icloud.com>
2017-01-21 14:20:21 +0800

    eh gud

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 var bgImg= document.createElement("img");
 var enemyImg= document.createElement("img");
+var buttonImg= document.createElement("img");
 
-bgImg.src= "images/rukia.gif";
+bgImg.src= "images/map.png";
 enemyImg.src= "images/slime.gif";
+buttonImg.src= "images/tower-btn.png";
 
 var canvas= document.getElementById("game-canvas");
 var ctx= canvas.getContext("2d");
 
 function draw (){
-  ctx.drawImage(enemyImg,enemy.x,enemy.y)
-  ctx.drawImage(bgImg,0,0)
+  ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(enemyImg,enemy.x,enemy.y);
+    ctx.drawImage(buttonImg,button.x,button.y,100,100)
 }
 
 setInterval (draw, 16)
@@ -18,3 +21,11 @@ var enemy={
   x:90,
   y:480-32
 }
+ var button={
+   x:540,
+   y:380
+ }
+
+ $("#target").mousemove(function (event) {
+   console.log("x:" + event.offsetX + ",y:" + event.offsetY);
+ });
