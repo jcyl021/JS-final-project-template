349f88d2b9253d4761daf92fd6b2a850e60b0681
jcyl021 <liu_jasper@icloud.com>
2017-01-21 13:55:06 +0800

    ugh

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var bgImg= document.createElement("img");
 var enemyImg= document.createElement("img");
+
 bgImg.src= "images/rukia.gif";
+enemyImg.src= "images/slime.gif";
+
 var canvas= document.gtElementByld("game-canvas");
 var ctx= canvas.getContext("2d");
 
 function draw (){
   ctx.drawImage(enemyImg,enemy.x,enemy.y)
+  ctx.drawImage(bgImg,0,0)
 }
 
 setInterval (draw, 16)
